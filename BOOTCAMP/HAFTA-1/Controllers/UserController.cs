d753c810606fd5f1036e59790ced9088bf55ec87
Berk Sayin <berkosayin@gmail.com>
2021-11-22 20:21:30 +0300

    UserController added. getuser, getuserbyid, insertuser implamented

---
+++ BOOTCAMP/HAFTA-1/Controllers/UserController.cs
@@ -0,0 +1,58 @@
+﻿using HAFTA_1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HAFTA_1.Controllers
+{
+    public class UserController : Controller
+    {
+        private static List<User> users = new List<User>() {
+            new User{
+                Id = 1,
+                UserEmail = "example1@mail.com",
+                UserName = "John Doe"
+            },
+            new User{
+                Id = 2,
+                UserEmail = "example2@mail.com",
+                UserName = "Jane Doe"
+            },
+            new User{
+                Id = 3,
+                UserEmail = "example3@mail.com",
+                UserName = "Max William"
+            },
+            new User{
+                Id = 4,
+                UserEmail = "example4@mail.com",
+                UserName = "Edgar Allan"
+            },
+            new User{
+                Id = 5,
+                UserEmail = "example5@mail.com",
+                UserName = "Charles Dickens"
+            }
+        };
+
+        [HttpGet("GetUsers")]
+        public List<User> GetUsers() {
+            return users;
+        }
+
+        [HttpGet("GetUserById")]
+        public User GetUser(int Id)
+        {
+            return users.Where(x => x.Id == Id).FirstOrDefault();
+        }
+
+        [HttpPost("InsertUser")]
+        public bool InsertUser([FromBody]User user)
+        {
+            users.Add(user);
+            return users.Exists(x => x.Id == user.Id);
+        }
+    }
+}
---
+++ BOOTCAMP/HAFTA-1/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HAFTA_1.Models
+{
+    public class User
+    {
+        [Required]
+        [Key]
+        [Range(0, int.MaxValue, ErrorMessage = "Please enter valid integer Number")]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(8, ErrorMessage = "Karakter sayısı 8'den büyük olamaz!")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string UserEmail { get; set; }
+    }
+}
