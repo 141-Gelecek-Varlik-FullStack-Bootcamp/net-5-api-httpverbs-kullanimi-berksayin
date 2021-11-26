0a6db439d4d8546f6a4e777843304f619eb19b05
Berk Sayin <berkosayin@gmail.com>
2021-11-26 14:54:16 +0300

    user model updated and controller completed

--- BOOTCAMP/HAFTA-1/Controllers/UserController.cs
+++ BOOTCAMP/HAFTA-1/Controllers/UserController.cs
@@ -9,50 +9,98 @@ namespace HAFTA_1.Controllers
 {
     public class UserController : Controller
     {
+        //Static users are generated when project started
         private static List<User> users = new List<User>() {
             new User{
                 Id = 1,
                 UserEmail = "example1@mail.com",
+                UserPassword = "12345678",
                 UserName = "John Doe"
             },
             new User{
                 Id = 2,
                 UserEmail = "example2@mail.com",
+                UserPassword = "a1b2c3d4",
                 UserName = "Jane Doe"
             },
             new User{
                 Id = 3,
                 UserEmail = "example3@mail.com",
+                UserPassword = "pass1234",
                 UserName = "Max William"
             },
             new User{
                 Id = 4,
                 UserEmail = "example4@mail.com",
+                UserPassword = "987654321",
                 UserName = "Edgar Allan"
             },
             new User{
                 Id = 5,
                 UserEmail = "example5@mail.com",
+                UserPassword = "charles1234",
                 UserName = "Charles Dickens"
             }
         };
 
+
+        //Listing all users
         [HttpGet("GetUsers")]
         public List<User> GetUsers() {
             return users;
         }
 
+
+        //Listing single user
         [HttpGet("GetUserById")]
         public User GetUser(int Id)
         {
             return users.Where(x => x.Id == Id).FirstOrDefault();
         }
 
+
+        //Adding a new user
         [HttpPost("InsertUser")]
-        public bool InsertUser([FromBody]User user)
+        public IActionResult InsertUser([FromBody]User user)
         {
+            var usertemp = users.SingleOrDefault(x => x.Id == user.Id);
+            if (usertemp == null) 
+            {
+                return BadRequest();
+            }
             users.Add(user);
-            return users.Exists(x => x.Id == user.Id);
+            return Ok();
         }
+
+
+        //Updating a user
+        [HttpPut("UpdateUser")]
+        public IActionResult UpdateUser(int id, [FromBody]User user)
+        {
+            var usertemp = users.FirstOrDefault(x => x.Id == user.Id);
+            if (usertemp == null)
+            {
+                return BadRequest();
+            }
+            usertemp.UserEmail = user.UserEmail;
+            usertemp.UserPassword = user.UserPassword;
+            usertemp.UserName = user.UserName;
+            return Ok();
+        }
+
+
+        //Deleting a user
+        [HttpDelete("DeleteUser")]
+        public IActionResult DeleteUser(int id) 
+        {
+            var usertemp = users.FirstOrDefault(x => x.Id == id);
+            if (usertemp == null)
+            {
+                return BadRequest();
+            }
+            users.Remove(usertemp);
+            return Ok();
+        }
+
     }
 }
--- BOOTCAMP/HAFTA-1/Models/User.cs
+++ BOOTCAMP/HAFTA-1/Models/User.cs
@@ -6,20 +6,28 @@ using System.Threading.Tasks;
 
 namespace HAFTA_1.Models
 {
+    //Main user model
     public class User
     {
         [Required]
         [Key]
-        [Range(0, int.MaxValue, ErrorMessage = "Please enter valid integer Number")]
+        [Range(0, int.MaxValue, ErrorMessage = "Please enter valid integer number.")]
         public int Id { get; set; }
-
-        [Required]
-        [StringLength(8, ErrorMessage = "Karakter sayısı 8'den büyük olamaz!")]
-        public string UserName { get; set; }
 
         [Required]
         [DataType(DataType.EmailAddress)]
         [EmailAddress]
         public string UserEmail { get; set; }
+
+        [Required]
+        [StringLength(8, ErrorMessage = "Maximum character must be 8!")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 8)]
+        [RegularExpression("^((?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])|(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[^a-zA-Z0-9])|(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[^a-zA-Z0-9])|(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^a-zA-Z0-9])).{8,}$", ErrorMessage = "Passwords must be at least 8 characters and contain at 3 of 4 of the following: upper case (A-Z), lower case (a-z), number (0-9) and special character (e.g. !@#$%^&*)")]
+        public string UserPassword { get; set; }
+
     }
 }
